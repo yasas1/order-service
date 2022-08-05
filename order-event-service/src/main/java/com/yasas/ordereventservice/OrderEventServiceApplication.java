87cfb0afa5297e13fabcbf6b5d7cea2990400666
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-08-06 00:47:31 +0530

    order-service and order-event-service were initialized

---
+++ order-event-service/src/main/java/com/yasas/ordereventservice/OrderEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yasas.ordereventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEventServiceApplication.class, args);
+	}
+
+}
